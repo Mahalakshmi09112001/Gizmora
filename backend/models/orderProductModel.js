4eff84b33496cab6cd7054775f3154994f9297a4
dmex-vikramathithan <vikramathithan@qnqsoft.com>
2025-04-05 20:04:42 +0530

    "add all"

--- backend/models/orderProductModel.js
+++ backend/models/orderProductModel.js
@@ -1,51 +1,38 @@
-const { default: mongoose } = require('mongoose');
-
+const { default: mongoose, model } = require('mongoose')
 const orderSchema = mongoose.Schema({
-    productDetails: {
-        type: Array,
-        default: []
+    productDetails : {
+        type : Array,
+        default : []
+
     },
-    email: {
+    email : {
+        type : String,
+        default : ""
+    },
+    userId : {
         type: String,
-        default: ""
+        default : ""
     },
-    userId: {
-        type: String,
-        default: ""
-    },
-    paymentDetails: {
-        paymentId: {
-            type: String,
-            default: ""
+    paymentDetails : {
+        paymentId : {
+            type : String,
+            default : ""
         },
-        payment_method_type: {
-            type: [String],
-            default: []
-        },
-        payment_status: {
-            type: String,
-            default: ""
+        payment_method_type : [],
+        payment_status : {
+            type : String,
+            default : ""
         }
     },
-    shipping_options: {
-        type: [String],
-        default: []
-    },
-    totalAmount: {
+    shipping_options : [],
+    totalAmount : {
         type: Number,
-        default: 0
-    },
-    address: {
-        fullName: { type: String, default: "" },
-        phone: { type: String, default: "" },
-        address: { type: String, default: "" },
-        city: { type: String, default: "" },
-        state: { type: String, default: "" },
-        pincode: { type: String, default: "" }
+        default : 0
     }
-}, {
-    timestamps: true
-});
 
-const orderModel = mongoose.model('order', orderSchema);
+},{
+    timestamps : true
+})
+
+const orderModel = mongoose.model('order',orderSchema)
 module.exports = orderModel;
